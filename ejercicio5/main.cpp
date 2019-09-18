5b059beef66d89d7e8f7f7e924c8dcba056b7510
miyulo139 <55298493+miyulo139@users.noreply.github.com>
2019-09-18 10:03:31 -0500

    Update main.cpp

--- ejercicio5/main.cpp
+++ ejercicio5/main.cpp
@@ -2,17 +2,24 @@
 #include "Tipos.h"
 using namespace std;
 
+
 int main() {
+    enteros num=0, pares=0,impares=0,can=0;
+    do{
+        cout << "Introduzca número: ";
+        cin >>num;
+        can++;
+        if (num%2==0)
+            pares++;
+        else
+            impares++;
+    }
+    while (num!=0);
 
-entero numero;
-caracter crt;
 
-cout << "ingrese el caracter:";
-cin >> crt;
-cout << "\ningrese el numero:";
-cin >> numero;
+    cout <<"Cantidad de números ingresados: "<< can-1<< "\n";
+    cout <<"Cantidad de números pares: "<< pares-1<< "\n";
+    cout <<"Cantidad de números impares: "<< impares<< "\n";
 
-// ToDO
-// implemente el algoritmo.
-  return 0;
+    return 0;
 }
