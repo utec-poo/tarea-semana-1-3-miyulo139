7164dbe04c91c24c4b97bd968ac17532250aa0be
miyulo139 <55298493+miyulo139@users.noreply.github.com>
2019-09-18 09:31:14 -0500

    Update main.cpp

--- ejercicio2/main.cpp
+++ ejercicio2/main.cpp
@@ -3,6 +3,25 @@
 using namespace std;
 
 int main() {
+    string zona, claro;
+    enteros precio=0;
+    cout << "Ingrese zona: ";
+    cin >> zona;
+    cout << "¿Es usted cliente Claro?: ";
+    cin >> claro;
+
+    if (zona== "Super Vip")
+        precio=212;
+    else if (zona== "Vip")
+        precio=170;
+    else if (zona== "Preferencial")
+        precio=136;
+    else if (zona== "General")
+        precio=59;
+
+    if (claro=="sí")
+        precio=precio-(precio*20/100);
+    cout << "Monto de entrada: " <<precio;
 
 
 // ToDO
