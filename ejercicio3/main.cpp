badfff432f27c903726e8551c25b366c8e777761
miyulo139 <55298493+miyulo139@users.noreply.github.com>
2019-09-18 09:52:05 -0500

    Update main.cpp

--- ejercicio3/main.cpp
+++ ejercicio3/main.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
 #include "Tipos.h"
+
 using namespace std;
 
 int main() {
 
-entero numero;
-caracter crt;
+    enteros numero;
+    caracter crt;
+    string color;
 
-cout << "ingrese el caracter:";
-cin >> crt;
-cout << "\ningrese el numero:";
-cin >> numero;
+    cout << "ingrese el caracter:";
+    cin >> crt;
+    cout << "\ningrese el numero:";
+    cin >> numero;
 
-// ToDO
-// implemente el algoritmo.
-  return 0;
+    if (crt == 'a' or 'c' or 'e' or 'g') {
+        if (numero % 2 == 1)
+            color = "black";
+        else
+            color = "white";
+    }
+    else {
+        if (numero % 2 == 0)
+        color = "black";
+        else
+        color = "white";
+    }
+
+cout <<"Corresponde a un casillero de color"<< color;
+    return 0;
 }
