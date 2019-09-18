20d20e1b493c013d049409328ca65291243fcfae
miyulo139 <55298493+miyulo139@users.noreply.github.com>
2019-09-18 10:15:09 -0500

    Update main.cpp

--- ejercicio6/main.cpp
+++ ejercicio6/main.cpp
@@ -3,15 +3,26 @@
 using namespace std;
 
 int main() {
+    enteros num=0, dig=0, pares=0, impares=0;
+    do{
+        cout << "Introduzca número: ";
+        cin >>num;
+    }
+    while (num<100);
 
-entero numero;
+    for (int i=0; num>0; i++){
+        if ((num%10)%2==0)
+            pares++;
+        else
+            impares++;
+        num/=10;
+        dig++;
+    }
 
 
-cout << "ingrese un numero:";
+    cout <<"Cantidad de dígitos: "<< dig<< "\n";
+    cout <<"Cantidad de dígitos pares: "<< pares<< "\n";
+    cout <<"Cantidad de dígitos impares: "<< impares<< "\n";
 
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
-  return 0;
+    return 0;
 }
